e69003c745fd5b348016f25f84136b69f4c262c9
AWcancom <andreas.wiesinger@cancom.com>
2025-12-15 15:19:01 +0100

    Update StartZT.ps1

--- StartNetScripts/StartZT.ps1
+++ StartNetScripts/StartZT.ps1
@@ -33,7 +33,7 @@ if ($DriverPack){
 }
 
 if ($Manufacturer -eq 'Lenovo') {
-    Install-Module -Name 'lsuclient' -Force -Confirm:$false
+    Install-Module -Name 'lsuclient' -Force -Confirm:$false -SkipPublisherCheck -TrustRepository
     $updates = get-lsupdate
     $updates | where-object {$_.type -eq 'BIOS'} | Install-LSUpdate -Verbose
 }
